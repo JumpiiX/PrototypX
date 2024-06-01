a972b4a70850b0395fd35d4f54bcd855e64a62e6
CodingNerd187 <unterguggenberger.david@gmail.com>
2024-06-02 01:25:15 +0200

    .exe

--- PrototypX/PrototypX.cpp
+++ PrototypX/PrototypX.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "PrototypX");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "PrototypX.exe");
     window.clear(sf::Color::Black);
 
     while (window.isOpen()) {
